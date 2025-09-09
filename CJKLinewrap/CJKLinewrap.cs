06959b36e0c6cae699cfad10c689596680fb6ec6
Yao Wei <medicalwei@gmail.com>
2025-09-09 19:39:15 +0800

    fix version

--- CJKLinewrap/CJKLinewrap.cs
+++ CJKLinewrap/CJKLinewrap.cs
@@ -18,7 +18,7 @@ using static OfficialAssets.Graphics;
 namespace CJKLinewrap;
 //More info on creating mods can be found https://github.com/resonite-modding-group/ResoniteModLoader/wiki/Creating-Mods
 public class CJKLinewrap : ResoniteMod {
-	internal const string VERSION_CONSTANT = "0.0.2"; //Changing the version here updates it in all locations needed
+	internal const string VERSION_CONSTANT = "0.1.1"; //Changing the version here updates it in all locations needed
 	public override string Name => "CJKLinewrap";
 	public override string Author => "Meow Wei 魏喵";
 	public override string Version => VERSION_CONSTANT;
